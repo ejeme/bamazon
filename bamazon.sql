28d027b08aad882559af381ce0171f8892df58e0
Ejeme <Ejeme@github.com>
2018-04-19 23:39:00 -0500

    Made changes to my syntax and added my sql file

---
+++ bamazon.sql
@@ -0,0 +1,39 @@
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("pasta","food",3.99,3);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("babyformula","food",11.99,12);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("toastedQuinoa","food",8.99,15);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("candy","food",2.99,30);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("cheetos","food",11.99,6);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("cookingOil","food",7.99,24);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("coffee","food",11.99,12);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("tea","food",11.99,8);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("oatbars","food",11.99,23);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("maplesyrup","food",11.99,32);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("nutella","food",11.99,14);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("goldfish","food",11.99,31);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("oreos","food",11.99,17);
+;
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,132 +1,98 @@
+
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var Table = require("cli-table");
 
-var resString = '';
-var resJSON = '';
+
+
 
 // Settings to connect to the Bamazon database
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'Bamazon'
+	host: "localhost",
+	port: 3306,
+	user: "root",
+	password: "",
+	database: "bamazon"
 });
-//created a connection with the database so I can work with the data 
+
+
+
+
+
+// Check to see if connection is successful
 connection.connect(function (err) {
-    if (err) throw (err);
-    console.log('connected as id $ {connection.threadId}');
+	if (err) throw err;
+	console.log('connection id', connection.threadId);
+	displayProducts();
 
-    connection.query("SELECT*FROM products", function (err, res) {
-        if (err) throw err;
-        console.log(res);
-    });
 
 });
-// Query Data
-function displayTable() {
-	
-	var query = 'SELECT * FROM products';
-		connection.query(query,function(err,res,fields) {
-			if(err) throw err;
-			//Converts to string
-			resString = JSON.stringify(res,null,2);
-			//Convert to JSON
-			resJSON  = JSON.parse(resString);
-			//Testing
-			var table = new Table({
-			    head: ['item_id', 'product_name','department_name','price','stock_quantity'], 
-			    colWidths: [25, 25, 25 ,25 ,25]
-			});
-			for(var i = 0; i < resJSON.length; i++) {
-				//Creates a new array
-				var newArray = new Array();
-				//adds content to table
-				table.push(newArray);
-				//Adds content to new array of Nth row
-				for(var j = 0; j < columns.length; j++){
-					newArray.push(resJSON[i][columns[j]]);
-				}
-			}
-			//Displays Table in terminal
-			console.log(table.toString());
-			customerRequest();
-		});
+//Query the database
+function displayProducts() {
+	connection.query("SELECT * FROM products", function (err, res) {
+		if (err)
+			console.log(err);
 
+
+
+
+		for (var i = 0; i < res.length; i++) {
+			console.log(res[i].item_id + "|" + res[i].product_name + "|" + res[i].price);
+		}
+		promptCustomer(res);
+	})
 }
+
+
+
 //working on displaying prompt
 
-inquirer.prompt([
-    {
+inquirer.prompt([{
+	type: "input",
+	name: "itemID",
+	message: "What is the ID of the product you would like to purchase?",
 
-        type: "input",
-        name: "itemid",
-        message: "What is the ID of the product you would like to purchase?",
-        validate: function (value) {
-            if (isNaN(value) == false && parseInt(value) <= res.length && parseInt(value) > 0) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-    },
+	type: "input",
+	name: "quantity",
+	message: "Please enter the quantity would you like to purchase?",
 
-    {
-        type: "input",
-        name: "itemQty",
-        message: "Please enter the quantity would you like to purchase?",
-        validate: function (value) {
-            if (isNaN(value)) {
-                return false;
-            } else {
-                return true;
-            }
-        }
-    }
+}]).then(function (answers) {
+	var selectedProduct;
+
+	for (var i = 0; i < res.length; i += 1) {
+		if (res[i].item_id === parseInt(answers.itemID)) {
+			selectedProduct = res[i];
+			console.log(res[i]);
 
 
 
-
-]).then(function (ans) {
-    var whatToBuy = (ans.id) - 1;
-    var howMuchToBuy = parseInt(ans.qty);
-    var grandTotal = parseFloat(((res[whatToBuy].Price) * howMuchToBuy).toFixed(2));
-
-
-});
-
-function checkQuantity(id, quantity) {
-	var query = "SELECT stock_quantity FROM products WHERE ?"
-
-	connection.query(query,{
-		item_id: id
-	}, 
-	function(err,res,fields) {
-		if(err) throw err;
-		var stockJSON = JSON.stringify(res,null,2);
-		var stockParsed = JSON.parse(stockJSON);
-		var stockQuantity = stockParsed[0].stock_quantity;
-		
-		//If quantity in Database is greater then users quantity request ....
-		if(stockQuantity >= quantity) {
-			var query = 'UPDATE products SET ? WHERE ?'
-			//Update database
-			connection.query(query,[
-			{
-				//Subtract quantity in database from users request quantity
-				stock_quantity: stockQuantity - quantity
-			},
-			{
-				//product id
-				item_id: id
-			
-			}], function(err,res,fields){
-				promptBool = false;
-				displayTable();
-			});
-		}	
-		else {
-			console.log('Insufficient quantity!');
 		}
-	});
-}
+	}
+
+
+
+
+})
+
+var userQuantity = parseInt(answers.quantity);
+
+if (selectedProduct.stock_quantity < userQuantity) {
+	console.log("Insufficient Quantity");
+} else {
+	//selectedProduct.price is coming from the database
+	var cost = userQuantity * selectedProduct.price
+	//.toFixed limits decimal value to 2 spots.
+	console.log("Your total cost is: $ " + cost.toFixed(2));
+
+	//To update database
+	connection.query("UPDATE products SET ? WHERE ?", [{ "stock_quantity": selectedProduct.stock_quantity - userQuantity }, { "item_id": answers.itemID }], function (err, res) {
+		if (err) {
+			console.log("There was an error. Purchase not made.");
+			throw err;
+		}
+		console.log("Thanks for your purchase!");
+
+	})
+};
+        
+    
+
